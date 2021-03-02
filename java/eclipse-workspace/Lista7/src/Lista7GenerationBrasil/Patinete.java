b24c437e400ee1a95044650b206e39732355bdf0
Bruno Bandeira <brunodev29ab@gmail.com>
2021-03-02 18:53:13 -0300

    Exercicio patinete, feito

--- java/eclipse-workspace/Lista7/src/Lista7GenerationBrasil/Patinete.java
+++ java/eclipse-workspace/Lista7/src/Lista7GenerationBrasil/Patinete.java
@@ -1,7 +1,36 @@
 package Lista7GenerationBrasil;
 
+import java.util.Scanner;
+
+import Lista7GenerationBrasilMetodos.PatineteMetodos;
+
 public class Patinete {
 	public static void main(String[] args) {
+		
+		String cor;
+		int anoDeFabricacao;
+		
+		PatineteMetodos p = new PatineteMetodos();
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Cor do patinete ? ");
+		cor = teclado.next();
+		
+		System.out.println("Ano de fabricação do patinete ?");
+		anoDeFabricacao = teclado.nextInt();
+		
+		
+		if(cor.equalsIgnoreCase("Branco") || cor.equalsIgnoreCase("Azul") || cor.equalsIgnoreCase("Vermelho")) {
+			System.out.println("O patinete na cor " + cor + " fabricado em " + anoDeFabricacao + " funciona ? " + p.isAndando());
+		}
+		
+		if(cor.equalsIgnoreCase("Preto") || cor.equalsIgnoreCase("Caramelo") || cor.equalsIgnoreCase("Roza")) {
+			System.out.println("O patinete na cor " + cor + " fabricado em " + anoDeFabricacao + " funciona ? " + p.isaAAndando());
+
+		}
+		
+		
+		
 		
 	}
 }
--- java/eclipse-workspace/Lista7/src/Lista7GenerationBrasilMetodos/PatineteMetodos.java
+++ java/eclipse-workspace/Lista7/src/Lista7GenerationBrasilMetodos/PatineteMetodos.java
@@ -1,10 +1,48 @@
 package Lista7GenerationBrasilMetodos;
 
 public class PatineteMetodos {
+	
 	private boolean andando = true;
 	private String cor;
 	private int anoDeFabricacao;
+
+	public PatineteMetodos() {
+
+	}
+
+	public PatineteMetodos(boolean andando, String cor, int anoDeFabricacao) {
+		super();
+		this.andando = andando;
+		this.cor = cor;
+		this.anoDeFabricacao = anoDeFabricacao;
+	}
 	
-	
+	public boolean isaAAndando() {
+		return false;
+	}
+
+	public boolean isAndando() {
+		return andando;
+	}
+
+	public void setAndando(boolean andando) {
+		this.andando = andando;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public int getAnoDeFabricacao() {
+		return anoDeFabricacao;
+	}
+
+	public void setAnoDeFabricacao(int anoDeFabricacao) {
+		this.anoDeFabricacao = anoDeFabricacao;
+	}
 
 }
